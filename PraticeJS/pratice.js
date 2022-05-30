acacd0e61e0fff10c5f202fe56229119565157a8
Nujan100 <nujan100@gmail.com>
2022-05-30 11:38:38 +0545

    Day13

--- PraticeJS/pratice.js
+++ PraticeJS/pratice.js
@@ -1,11 +1,45 @@
-
-  function addTwoNumbers(numOne, numTwo) {
-    this.numOne = numOne;
-    this.numTwo = numTwo;
-    let total = numOne + numTwo
-    return total
-
+class calculator{
+  firstNum=null;
+  secondNum=null;
+  resultNum=null;
+  constructor(firstNum, secondNum){
+    this.firstNum = firstNum;
+    this.secondNum = secondNum;
+    this.resultNum = 0;
+  }
+  setfirstNum (firstNum){
+    this.firstNum = firstNum;
+  }
+  
+  setsecondNum (secondNum){
+    this.secondNum = secondNum;
+  }
+  getfirstNum(){
+    return this.firstNum;
+  }
+  getsecondNum(){
+    return this.secondNum;
+  }
+  getresultNum(){
+    return this.resultNum;
+  }
 }
 
-console.log(addTwoNumbers(20,5));
-document.write(addTwoNumbers(20,5));
+class calc{
+  myCalculator = null;
+  constructor(myCalculator){
+    this.myCalculator = myCalculator;
+  }
+  setmyCalculator(myCalculator){
+    this.myCalculator = myCalculator;
+  }
+  getmycalculator(){
+    return this.myCalculator;
+  }
+  sum(){
+    this.myCalculator.resultNum = this.myCalculator.getfirstNum()+this.myCalculator.getsecondNum();
+  } 
+  toString(){
+    return(this.firstNum+","+this.secondNum+","+this.result);
+}
+}
