1a27cd05df45d3a7c77e40ce043acfbd66769750
Nujan100 <nujan100@gmail.com>
2022-06-20 18:02:00 +0545

    JavaScript-Day28

--- PraticeJS/pratice.js
+++ PraticeJS/pratice.js
@@ -1,17 +1,6 @@
-var PM = 40;
-var process1 = function(sub1, sub2, sub3){
-    var total = Number(sub1)+ Number(sub2)+ Number(sub3);
-    console.log(sub1, sub2, sub3);
-    var avg = total / 3;
-    var result = "FAIL";
-    if(sub1 >= PM && sub2 >= PM && sub3 >= PM){
-        result = "PASS";
-    }
-    
-    obj1 ={
-        total:total,
-        avg:avg,
-        result:result
-    }
-    return obj1;
+var elements = document.getElementsByTagName("p");
+for (let i = 0; i < elements.length; i++) {
+    console.log(elements[i].innerHTML);
 }
+console.log(elements.length);
+document.getElementById("p4").innerText = "Welcome to broadway";
